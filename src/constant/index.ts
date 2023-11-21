232fd9c1d8c3e1d7541aae223a049e1cfb639a35
Krishna Agarwal <krishna@gkmit.co>
2023-11-21 17:03:40 +0530

    refractor:corrected-intendation;added-admin-constant

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -4,9 +4,12 @@ import {
   Assessment as AssessmentIcon,
   RestaurantMenu as RestaurantMenuIcon,
   BedroomParent as BedroomParentIcon,
+  AssignmentIndRounded as AssignmentIndRoundedIcon,
 } from "@mui/icons-material";
 
-import { SidebarValueType } from "@ts/types";
+import { SidebarValueType, AdminDashboardDetailsTypes } from "@ts/types";
+
+export const ADMIN_ROLE: string = "admin";
 
 export const ADMIN_SIDEBAR: SidebarValueType[] = [
   {
@@ -55,3 +58,39 @@ export const STUDENT_SIDEBAR: SidebarValueType[] = [
 ];
 
 export const SETTINGS: string[] = ["Profile", "Logout"];
+
+export const USER_DATA_URL: string =
+  "https://api.npoint.io/de4471c76c2129b205d7";
+
+export const ADMIN_DASHBOARD_DETAIL_URL: string =
+  "https://api.npoint.io/afae6426adf49484df48";
+
+export const NOTICES_URL: string = "https://api.npoint.io/dedd459425f10436406b";
+
+export const ADMIN_DASHBOARD_DETAIL: AdminDashboardDetailsTypes[] = [
+  {
+    label: "Students",
+    icon: AccountBoxIcon,
+    field: "numberOfStudents",
+    color: "bg-primary-main",
+  },
+  {
+    label: "Complaints",
+    icon: AssessmentIcon,
+    field: "complaitsPending",
+    color: "bg-colors-lightBlue",
+  },
+
+  {
+    label: "Rooms",
+    icon: BedroomParentIcon,
+    field: "numberOfRooms",
+    color: "bg-colors-lightGreen",
+  },
+  {
+    label: "Staff",
+    icon: AssignmentIndRoundedIcon,
+    field: "numberOfStaff",
+    color: "bg-secondry-main",
+  },
+];
--- src/ts/types.ts
+++ src/ts/types.ts
@@ -12,3 +12,22 @@ export type UserDetialsType = {
   email: string;
   role: string;
 };
+
+export type AdminDashboardDataTypes = {
+  numberOfStudents: number;
+  complaitsPending: number;
+  numberOfRooms: number;
+};
+
+export type AdminDashboardDetailsTypes = {
+  label: string;
+  icon: FC<SvgIconProps>;
+  field: string;
+  color: string;
+};
+
+export type NoticeDataType = {
+  date: string;
+  heading: string;
+  content: string;
+};
